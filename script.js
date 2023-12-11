925fde0ea50771d0196301cbb7e360b1ce4b6e3c
Uday Bari <115573575+UdayBari3421@users.noreply.github.com>
2023-12-11 12:35:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,7 +19,15 @@ Employee.protype.constructor = Employee;
 Employee.prototype.jobGreet = function () {
   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 };
-	
+
+
+const person1 = new Person('Alice', 25);
+person1.greet(); // Output: Hello, my name is John, I am 30 years old.
+
+const employee1 = new Employee('Bob', 30, 'Manager.');
+employee1.greet(); 
+employee1.jobGreet();
+
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
